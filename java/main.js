09f911eb49a6b4e5940b1e17992e6dcb5ba735b0
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-09-13 20:18:17 +0300

    adds mailchimp

--- java/main.js
+++ java/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         $("#imageofdesign, #designdetails").toggle();
     })
     // toggle in the product image and on clicking image the development details appear
-    $(".dev").click(function () {
+    $(".development").click(function () {
         $("#imageofdevelopment, #developmentdetails").toggle();
     })
     // toggle in the product image and on clicking image the product details appear
